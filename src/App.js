5220a5ae9708fbef053eb6f70de0722b2e8275e6
brunapicoli <brunatpicoli@outlook.com>
2019-10-20 01:13:12 -0300

    layout com algumas funções

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Provider } from 'react-redux';
 import Routes from './routes';
+import {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
+
 function App() {
+  const [latitude, setLatitude] = useState("22.25")
+  const [longitude, setLongitude] = useState("45.27")
+  console.log(latitude);
+
+  function handleSubmit(){
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Latitude: </h3>
-        <input />
+        <input value={latitude} onChange={event => setLatitude(event.target.value)}/>
         <br />
         <h3>Longitude: </h3>
-        <input />
+        <input value={longitude} onChange={event => setLongitude(event.target.value)}/>
         <br /> <br />
         <button type="submit">OK</button>
       </form>
